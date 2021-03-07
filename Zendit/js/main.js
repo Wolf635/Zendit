6d1fd00e8c817a7eebc3915d2ee323671c615c0e
Spartak <60838718+Wolf635@users.noreply.github.com>
2021-03-07 21:07:11 +0300

    Delete Zendit directory

--- Zendit/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.nav__item').click(function (event) {
-    if ($('.block').hasClass('one')) {
-      $(".nav__item").not($(this)).removeClass('active');
-      $(".nav__item--text").not($(this).next()).slideUp(300);
-    }
-    $(this).toggleClass('active').next().slideToggle(300);
-  });
-});
